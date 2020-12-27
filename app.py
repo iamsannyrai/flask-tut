f1fd768f06911c74b01c79dab90b487d9c9141bc
office <iam.sannyrai@gmail.com>
2020-12-27 19:53:54 +0545

    uses template to render a page

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 # imports Flask and create an instance of the Flask object
 from flask import Flask
+from flask import render_template # render_template helps to load a template
+from datetime import datetime
+
 # __name__ is a built-in variable which evaluates to the name of the current module
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, Flask!"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_there(name = None):
+    return render_template(
+        'hello_there.html',
+        name=name,
+        date=datetime.now()
+    )
 
 """ 
     if we want to use a different filename than app.py
